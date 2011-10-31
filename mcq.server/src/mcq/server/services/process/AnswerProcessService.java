ff68e8af9c61277c14736e19d4f54baf64dc0193
jmini <dev@jmini.fr>
2011-10-31 17:17:14 +0100

    (Step G.1) AnswerProsses Service - prepareCreate()

--- mcq.server/src/mcq/server/services/process/AnswerProcessService.java
+++ mcq.server/src/mcq/server/services/process/AnswerProcessService.java
@@ -24,37 +24,47 @@ import mcq.shared.services.process.IAnswerProcessService;
 
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.commons.exception.VetoException;
+import org.eclipse.scout.rt.server.services.common.jdbc.SQL;
 import org.eclipse.scout.rt.shared.services.common.security.ACCESS;
 import org.eclipse.scout.service.AbstractService;
 
 public class AnswerProcessService extends AbstractService implements IAnswerProcessService {
 
+  @Override
   public AnswerFormData prepareCreate(AnswerFormData formData) throws ProcessingException {
     if (!ACCESS.check(new CreateAnswerPermission())) {
       throw new VetoException(Texts.get("AuthorizationFailed"));
     }
-    //TODO [jebr] business logic here
+
+    SQL.selectInto(" select question_text " +
+        " from  questions " +
+        " where question_id = :QuestionNr " +
+        " into  :questionText", formData);
+
     return formData;
   }
 
+  @Override
   public AnswerFormData create(AnswerFormData formData) throws ProcessingException {
     if (!ACCESS.check(new CreateAnswerPermission())) {
       throw new VetoException(Texts.get("AuthorizationFailed"));
     }
     //TODO [jebr] business logic here.
-   return formData;
+    return formData;
   }
 
-  public AnswerFormData load(AnswerFormData formData) throws ProcessingException{
-    if(!ACCESS.check(new ReadAnswerPermission())){
+  @Override
+  public AnswerFormData load(AnswerFormData formData) throws ProcessingException {
+    if (!ACCESS.check(new ReadAnswerPermission())) {
       throw new VetoException(Texts.get("AuthorizationFailed"));
     }
     //TODO [jebr] business logic here
     return formData;
   }
 
-  public AnswerFormData store(AnswerFormData formData) throws ProcessingException{
-    if(!ACCESS.check(new UpdateAnswerPermission())){
+  @Override
+  public AnswerFormData store(AnswerFormData formData) throws ProcessingException {
+    if (!ACCESS.check(new UpdateAnswerPermission())) {
       throw new VetoException(Texts.get("AuthorizationFailed"));
     }
     //TODO [jebr] business logic here
